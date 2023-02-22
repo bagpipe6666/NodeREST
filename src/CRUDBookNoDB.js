53cfc9c30af4a32d67fb47c3bf49664cbcb7a358
bagpipe6666 <bumpoloyou@gmail.com>
2023-02-22 10:05:59 +0700

    up

--- src/CRUDBookNoDB.js
+++ src/CRUDBookNoDB.js
@@ -1,62 +1,67 @@
-require("dotenv").config();
-const express = require('express');
+require("dotenv").config()
+const express = require("express")
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
-let books =[
+let books = [
     {
-        id:1,
-        title:'Book 1',
+        id: 1,
+        title: 'book1',
         author: 'Author 1'
     },
     {
-        id:2,
-        title:'Book 2',
+        id: 2,
+        title: 'book2',
         author: 'Author 2'
     },
     {
-        id:3,
-        title:'Book 3',
+        id: 3,
+        title: 'book3',
         author: 'Author 3'
     }
-];
+]
 
-app.get('/books',(req, res) => {
-    res.json(books);
-});
+app.get('/books', (req, res) => {
+    res.json(books)
+})
 
-app.get('/books/:id',(req, res) => {
-    const book = book.find(b => b.id === parseInt(req.params.id));
-    if (!book) res.status(404).send('Book not fount');
-    res.json(book);
-});
+app.get('/books/:id', (req, res) => {
 
-app.post('/books', (req ,res) =>{
-    const book ={
+    const book = books.find(b => b.id === parseInt(req.params.id))
+    if (!book) res.status(404).send("book not found")
+    res.json(book)
+
+})
+
+app.post('/books', (req, res) => {
+
+    const book = {
         id: books.length + 1,
         title: req.body.title,
         author: req.body.author
-    };
-    book.push(book);
-    res.send(book);
-});
+    }
+    books.push(book)
+    res.send(book)
 
-app.put('/books/:id',(req, res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if (!book) res.status(404).send('Bool not fount');
+})
+
+app.put('/books/:id', (req, res) => {
+
+    const book = books.find(b => b.id === parseInt(req.params.id))
+    if (!book) res.status(404).send("book not found")
     book.title = req.body.title
-    book.author = req.body.title
-    res.send(book);
-});
+    book.author = req.body.author
+    res.json(book)
+})
 
-app.delete('/books/:id',(req, res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if (!book) res.status(404).send('Bool not fount');
-    const index = book.index.indexOf(book);
-    books.splice(index,1);
-    res.send(book);
-});
+app.delete('/books/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id))
+    if (!book) res.status(404).send("book not found")
+    const index = books.indexOf(book)
+    books.splice(index, 1)
+    res.json(book)
+})
 
-const port = process.env.POST || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}...`))
